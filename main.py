7e3079fe2f8941923e8fe770fca5a5f29fc5cf39
serena78 <116153719+serena78@users.noreply.github.com>
2022-11-12 17:12:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ monlabel = "Quel texte analyser ? "
 options = pd.DataFrame(['Avis dataset', 'Texte libre'])
 
 
-n_topic = st.number_input(label= "Le nombre de topics", min_value=0, max_value=15)
+n_topics = st.number_input(label= "Le nombre de topics", min_value=0, max_value=15)
 if st.button(label = "Détecter le sujet d'insatisfaction") == True :
     prediction(model_pred, vectorizer, n_topics, text)
 
