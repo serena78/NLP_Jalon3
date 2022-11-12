b3935db9387ff207f6e4a3cbe8c3beef3e79797c
serena78 <116153719+serena78@users.noreply.github.com>
2022-11-12 17:37:14 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -118,32 +118,31 @@ model_pred = load(file_name2)
 
 from textblob import TextBlob
 import numpy as np
-def prediction(model, vectorizer, n_topic, new_reviews):
-    new_reviews=preprocess_text(new_reviews)
-    blob = TextBlob(new_reviews)
-    sentimentBlob = blob.sentiment.polarity
-    new_reviews = [new_reviews]
-    new_reviews_transformed = vectorizer.transform(new_reviews)
 
-    prediction = model.transform(new_reviews_transformed)
+def prediction(model,vectorizer,n_topic,new_reviews):
+  new_reviews=preprocess_text(new_reviews)
+  blob=TextBlob(new_reviews)
+  sentimentBlob=blob.sentiment.polarity
+  new_reviews = [new_reviews]
+  new_reviews_transformed=vectorizer.transform(new_reviews)
 
-    topics = ['Esthétique du cadre', 'Qualité de la sauce', 'Qualité de la pizza',
-              'Qualité du service au niveau de la prise de commande', 'Rapidité du sercive', 'Staff du restaurant',
-              'Description des burgers',
-              "Temps d'attente", "Menu poulet", "Boissons disponibles au bar",
-              "Comparaison par rapport à un autre passage dans le restaurant", "Plainte des clients au manager",
-              "Garniture sandwich", "Menu sushi",
-              "Probabilité de revenir dans le restaurant"]
-    if sentimentBlob < 0 and sentimentBlob > -1:
+  prediction= model.transform(new_reviews_transformed)
+ 
+  topics=['Esthétique du cadre','Qualité de la sauce','Qualité de la pizza','Qualité du service au niveau de la prise de commande','Rapidité du sercive','Staff du restaurant','Description des burgers',
+"Temps d'attente","Menu poulet","Boissons disponibles au bar","Comparaison par rapport à un autre passage dans le restaurant","Plainte des clients au manager","Garniture sandwich","Menu sushi",
+  "Probabilité de revenir dans le restaurant"]
+  if sentimentBlob<0 and sentimentBlob>-1:
+ 
+    max = np.argsort(prediction)
+    max_list=(list(max[0]))
+    max_list.reverse()
+    print(max_list)
+    topic=[]
+    for i in range(n_topic):
+      topic.append(topics[max_list[i]])  
+    return sentimentBlob,prediction,topic
 
-        max = np.argsort(prediction)
-        max_list = (list(max[0]))
-        max_list.reverse()
-        print(max_list)
-        topic = []
-        for i in range(n_topic):
-            topic.append(topics[max_list[i]])
-        return sentimentBlob, prediction, topic
+  return sentimentBlob,"C'est un avis positif"
 
-    return sentimentBlob,"C'est un avis positif"
+   
 
