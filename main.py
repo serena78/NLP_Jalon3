52202eb91941d2f1836da300adec9b43126843ed
serena78 <116153719+serena78@users.noreply.github.com>
2022-11-12 16:49:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ options = pd.DataFrame(['Avis dataset', 'Texte libre'])
 st.number_input(label= "Le nombre de topics", min_value=0, max_value=15)
 
 if st.button(label = "Détecter le sujet d'insatisfaction") == True :
-    prediction(model, vectorizer, n_topics, text)
+    prediction(model_pred, vectorizer, n_topics, text)
 
 
 with st.sidebar:
